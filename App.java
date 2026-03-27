85942f3d5825fc18e676f1f03c23b5403b2d224a
noorfatim4 <noorfatim4.n@gmail.com>
2026-03-27 10:35:32 +0100

    Docs: add file header comment

--- App.java
+++ App.java
@@ -1,3 +1,4 @@
+//Simple console calculator
 public class App {
   static void run() {
     System.out.println("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0 Exit");
--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,11 @@
 public class Calculator {
+
+    /**
+     * Returns the sum of a and b.
+     * @param a
+     * @param b
+     * @return
+     */
     public static double add(double a, double b) {
         return a + b;
     }
