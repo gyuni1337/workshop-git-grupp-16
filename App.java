2b8c6d1bc0522f2c32280e02181d4faebf5dd654
noorfatim4 <noorfatim4.n@gmail.com>
2026-03-27 09:54:38 +0100

    Use Calculator.add() plavceholder in App

--- App.java
+++ App.java
@@ -3,6 +3,7 @@ public class App {
     System.out.println("1) Subtract/n0) Exit");
       System.out.println("Your choice: ");
       String choice= new java.util.Scanner(System.in).nextLine();
+      if ("1".equals(choice))
+          System.out.println(Calculator.add(0, 0));
   }
-
 }
