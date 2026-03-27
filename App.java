756c5b2c3d36fbf471cde9a0c17dce8af6e4288f
noorfatim4 <noorfatim4.n@gmail.com>
2026-03-27 09:59:09 +0100

    Menu: include Multiply

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
   static void run() {
-    System.out.println("1) Subtract/n0) Exit");
+    System.out.println("1) Add\n2) Subtract\n3) Multiply\n0 Exit");
       System.out.println("Your choice: ");
       String choice= new java.util.Scanner(System.in).nextLine();
       if ("1".equals(choice))
