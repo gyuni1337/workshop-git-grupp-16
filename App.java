8e80e3ead11d3c471aac3c1a07a0b569d059d5e1
Shaima033 <shaima.almoayed33@gmail.com>
2026-03-27 09:39:01 +0100

    Menu:add'Subtract'+choice prompt

--- App.java
+++ App.java
@@ -1,5 +1,8 @@
 public class App {
   static void run() {
-    System.out.println("Calculator v0.1");
+    System.out.println("1") Subtract/n0) Exit")";
+      System.out.println("Your choice: ");
+      String choice= new java.util.Scanner(System.in).nextLine();
   }
+
 }
