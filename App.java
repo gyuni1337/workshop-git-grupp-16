fc087eddff17b0340f48fdfd748722a807fda6cf
Hamdi Ahmed <hamdeey.ahmed@gmail.com>
2026-03-27 09:27:27 +0100

    Menu:add 'Add' + choise prompt

--- App.java
+++ App.java
@@ -1,5 +1,9 @@
 public class App {
   static void run() {
-    System.out.println("Calculator v0.1");
+    System.out.println("1)Add/n0 Exit");
+
+    System.out.print("Choice: ");
+
+    String choice = new java.utile.Scanner(System.in).nextLine();
   }
 }
