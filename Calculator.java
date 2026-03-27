7af30ecf0a4e6205d7fba1a6fd94f707dacad9fc
noorfatim4 <noorfatim4.n@gmail.com>
2026-03-27 10:42:18 +0100

    Fix: correct sub() implementation

--- Calculator.java
+++ Calculator.java
@@ -11,7 +11,9 @@ public class Calculator {
     }
 
   /** Returns the difference of a and b. */
-  public static double sub(double a, double b) { return a+b;}
+  public static double sub(double a, double b) {
+      return a-b;
+  }
     /** Returns the product of javadoc for mul()*/
     public static double mul(double a,double b){
         return a*b;
