89fdd8ec5f8696639db66ec1b5e6e652322db117
gyuni1337 <gyuni1337@gmail.com>
2026-03-27 10:51:03 +0100

    Refactor: rename choice variable to option

--- App.java
+++ App.java
@@ -5,7 +5,7 @@ public class App {
       
     System.out.println("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0 Exit Program");
       System.out.println("Your choice: ");
-      String choice= new java.util.Scanner(System.in).nextLine();
+      String option = new java.util.Scanner(System.in).nextLine();
     double a = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
     double b = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
       if ("1".equals(option))
