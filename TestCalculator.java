591117106067764253d65bbd8775759200fecfbc
Shaima033 <shaima.almoayed33@gmail.com>
2026-03-27 10:42:55 +0100

    Test:add sub()test

--- TestCalculator.java
+++ TestCalculator.java
@@ -2,4 +2,7 @@ public class TestCalculator {
     public static void main(String[] args) {
         if(Calculator.add(2, 3) !=5) throw new AssertionError();
     }
+
+    if(Calculator.cub(5,2) !=3) throw new AssertionError();
+    
 }
